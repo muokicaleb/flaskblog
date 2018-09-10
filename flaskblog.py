2111a3ab4508bc3d6f2c5efc8262b24b86bbfe55
muokicaleb <muokicaleb@gmail.com>
2018-09-10 08:57:52 +0300

    user class and post class

--- flaskblog.py
+++ flaskblog.py
@@ -17,12 +17,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    image_file = db.Column(db.String(20), nullable=True, default='default.jpeg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}','{self.username}','{self.image_file}')"
+        return f"User('{self.username}', '{self.email}','{self.image_file}')"
 
 
 class Post(db.Model):
