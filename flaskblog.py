a7b4b00cd7acf7e27b30262e39a41527aee2b659
muokicaleb <muokicaleb@gmail.com>
2018-09-10 08:28:45 +0300

    setting up the db added user class and post class

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,39 @@
+from datetime import datetime
 from flask import Flask
 from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'ef93b926b18007527b4ed39170fee9fa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}','{self.username}','{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 posts = [
@@ -21,22 +47,6 @@ posts = [
         'author': 'Jane Doe',
         'title': 'second Post',
         'content': 'second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
 ]
