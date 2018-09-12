5477f3938b63ec6ab551de7462acd5be5af7dec2
muokicaleb <muokicaleb@gmail.com>
2018-09-12 09:05:09 +0300

    .

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=True, default='default.jpeg')
+    image_file = db.Column(db.String(20), nullable=True, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
