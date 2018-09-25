e7f8a24429e82eb97b7397ec0275fa9e82557705
muokicaleb <muokicaleb@gmail.com>
2018-09-25 08:20:27 +0300

    cleaned imports

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
 from flaskblog.config import Config
 
 
